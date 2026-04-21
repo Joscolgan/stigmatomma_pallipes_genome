cbdb0bc5bd7cea61ab72fdc671dccb1622909a2e
Joe Colgan <t.colgan@qmul.ac.uk>
2026-04-21 18:23:59 +0200

    Update run_nanoplot.sh

--- 01_quality_assessment/run_nanoplot.sh
+++ 01_quality_assessment/run_nanoplot.sh
@@ -12,15 +12,7 @@
 #SBATCH -v
 #SBATCH -o run_nanoplot.%j.out
 #SBATCH -e run_nanoplot.%j.err
-module purge
-#module load bio/Flye/2.9.1-GCC-11.2.0
-
-#source /home/tcolgan/meth_werk/bin/activate
-#source meth_werk/bin/activate
-#module load bio/bwa-meth/0.2.2-iccifort-2019.5.281
-#module load lang/Python/3.10.8-GCCcore-12.2.0
-#module load bio/BWA/0.7.17
-#module load bio/SAMtools/1.14-GCC-11.2.0 
+module purge 
 module load lang/Python/3.12.3-GCCcore-13.3.0   
 
 PROJECT_DIR=/lustre/project/m2_jgu-funcpoll
