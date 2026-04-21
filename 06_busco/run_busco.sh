5d20cc17070c2d88fb91c746337ebe4a2c2ec3ee
Joe Colgan <t.colgan@qmul.ac.uk>
2026-04-21 18:28:43 +0200

    Update run_busco.sh

--- 06_busco/run_busco.sh
+++ 06_busco/run_busco.sh
@@ -13,24 +13,10 @@
 #SBATCH -o run_busco.%j.out
 #SBATCH -e run_busco.%j.err
 module purge
-#module load bio/Flye/2.9.1-GCC-11.2.0
 
-#source /home/tcolgan/meth_werk/bin/activate
-#source meth_werk/bin/activate
-#module load bio/bwa-meth/0.2.2-iccifort-2019.5.281
-#module load lang/Python/3.10.8-GCCcore-12.2.0
-#module load bio/BWA/0.7.17
-#module load bio/SAMtools/1.14-GCC-11.2.0 
 module load bio/BUSCO/5.4.3-foss-2021b
 
 PROJECT_DIR=/lustre/project/m2_jgu-funcpoll
-
-## Check arguments are provided:
-if [ $# -eq 0 ]
-  then
-    echo "No arguments supplied. Usage: ./run_bwamem2_index.sh input.fasta"
-
-fi
 
 INPUT=$1
 DATABASE=$2
