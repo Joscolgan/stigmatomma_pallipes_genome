09e3a700ef589c58e88b6cd07069a85c22c25fd3
Joe Colgan <t.colgan@qmul.ac.uk>
2026-04-21 18:27:29 +0200

    Update run_minimap2.sh

--- 05_polishing/run_minimap2.sh
+++ 05_polishing/run_minimap2.sh
@@ -13,25 +13,11 @@
 #SBATCH -o run_minimap2.%j.out
 #SBATCH -e run_minimap2.%j.err
 module purge
-#module load bio/Flye/2.9.1-GCC-11.2.0
-
-#source /home/tcolgan/meth_werk/bin/activate
-#source meth_werk/bin/activate
-#module load bio/bwa-meth/0.2.2-iccifort-2019.5.281
-#module load lang/Python/3.10.8-GCCcore-12.2.0
-#module load bio/BWA/0.7.17
 module load bio/SAMtools/1.14-GCC-11.2.0 
 
 PROJECT_DIR=/lustre/project/m2_jgu-funcpoll
 
 INPUT=$1
 FASTQ=$2
-
-## Check arguments are provided:
-if [ $# -eq 0 ]
-  then
-    echo "No arguments supplied. Usage: ./run_bwamem2_index.sh input.fasta"
-
-fi
 
 src/minimap2-2.28_x64-linux/minimap2 -t 20 -x map-ont -t 20 "$INPUT" "$FASTQ" > results/minimap2_assembly_filt
